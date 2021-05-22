72541275fa2c72b7da6b9f53f1df9034334078c8
Jacques de Vos <jacques@nreality.com>
2021-05-22 12:32:54 +0200

    Revert "refactor: use calc function instead of calculate"
    This reverts commit a0ea1b420728dbe567e8daf9420429a829424948.

--- public/calc.js
+++ public/calc.js
@@ -2,25 +2,25 @@
 let plus;
 let minus;
 
-function calc(a, b, operator) {
-  if (operator === '+') {
-    return a + b;
-  }
-  return NaN;
-}
-
 function initialize() {
   [plus, minus] = document.calc.operator.options;
 }
 function calculate() {
   const a = parseInt(document.calc.val1.value, 10);
   const b = parseInt(document.calc.val2.value, 10);
-  let operator = '';
   if (plus.selected) {
-    operator = '+';
+    document.calc.answer.value = a + b;
   }
+  if (minus.selected) {
+    document.calc.answer.value = a - b;
+  }
+}
 
-  document.calc.answer.value = calc(a, b, operator);
+function calc(a, b, operator) {
+  if (operator === '+') {
+    return a + b;
+  }
+  return NaN;
 }
 
 module.exports = calc;
