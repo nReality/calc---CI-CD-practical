62cfacac255441d184141216feddff41beadb88a
Jacques de Vos <jacques@nreality.com>
2021-05-22 13:01:45 +0200

    feat: implement minus

--- public/calc.js
+++ public/calc.js
@@ -10,6 +10,9 @@ function calc(a, b, operator) {
   if (operator === '+') {
     return a + b;
   }
+  if (operator === '-') {
+    return a - b;
+  }
   return NaN;
 }
 
--- public/calc.test.js
+++ public/calc.test.js
@@ -16,5 +16,14 @@ test('adding two numbers should give the sum', () => {
   expect(result).toBe(3);
 });
 
-// subtracting two number should give the difference
-// given(precondition/input) 5 and 3 when(action) MINUS then(postcondition vs expectation) get 2
+test('subtracting two number should give the difference', () => {
+  // given
+  const firstNumber = 5;
+  const secondNumber = 3;
+
+  // when
+  const result = calc(firstNumber, secondNumber, '-');
+
+  // then
+  expect(result).toBe(2);
+});
