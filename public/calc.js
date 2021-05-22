66ae6ca5d68696793c035260a3b06a66a8a8176a
Jacques de Vos <jacques@nreality.com>
2021-05-22 12:57:51 +0200

    refactor: move calc into seperate function

--- public/calc.js
+++ public/calc.js
@@ -5,22 +5,23 @@ let minus;
 function initialize() {
   [plus, minus] = document.calc.operator.options;
 }
-function calculate() {
-  const a = parseInt(document.calc.val1.value, 10);
-  const b = parseInt(document.calc.val2.value, 10);
-  if (plus.selected) {
-    document.calc.answer.value = a + b;
-  }
-  if (minus.selected) {
-    document.calc.answer.value = a - b;
-  }
-}
 
 function calc(a, b, operator) {
   if (operator === '+') {
     return a + b;
   }
   return NaN;
+}
+
+function calculate() {
+  const a = parseInt(document.calc.val1.value, 10);
+  const b = parseInt(document.calc.val2.value, 10);
+  if (plus.selected) {
+    document.calc.answer.value = calc(a, b, '+');
+  }
+  if (minus.selected) {
+    document.calc.answer.value = calc(a, b, '-');
+  }
 }
 
 module.exports = calc;
--- public/calc.test.js
+++ public/calc.test.js
@@ -1,7 +1,20 @@
 /* eslint-disable no-undef */
 const calc = require('./calc.js');
 
-test('calc should add two numbers given plus operator', () => {
-  const result = calc(1, 2, '+');
+// adding two numbers should give the sum
+// given(precondition/input) 1 and 2 when(action) ADD then(postcondition vs expectation) get 3
+
+test('adding two numbers should give the sum', () => {
+  // given
+  const firstNumber = 1;
+  const secondNumber = 2;
+
+  // when
+  const result = calc(firstNumber, secondNumber, '+');
+
+  // then
   expect(result).toBe(3);
 });
+
+// subtracting two number should give the difference
+// given(precondition/input) 5 and 3 when(action) MINUS then(postcondition vs expectation) get 2
